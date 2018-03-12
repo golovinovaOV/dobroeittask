816961e08bfea6cf329fd706e3ba791f8e48fe13
Oksana Holovinova <golovinova.o.v@gmail.com>
2018-03-12 21:29:20 +0200

    Updated Task 4 for week 2.

--- Week2/src/com/holovinova/Task4.java
+++ Week2/src/com/holovinova/Task4.java
@@ -2,23 +2,45 @@ package com.holovinova;
 
 public class Task4 {
 	public static void main(String[] args) {
-		int number = 9;
-		// get factorial using iteration algorithm
-		System.out.println("Iteration. "+ number + "! = " + iterationFactorial(number));
-		// get factorial using recursive algorithm
-		System.out.println("Recursive. "+ number + "! = " + recursiveFactorial(number));
+		int number = -9;
+		long iterationResult;
+		long recursiveresult;
+		
+		try {			
+			// get factorial using recursive algorithm
+			recursiveresult = recursiveFactorial(number);
+			System.out.println("Recursive. " + number + "! = " + recursiveresult);			
+		}catch (IllegalArgumentException e) {
+			System.out.println(e.getMessage());
+		}
+		
+		try {
+			// get factorial using iteration algorithm
+			iterationResult = iterationFactorial(number);
+			System.out.println("Iteration. " + number + "! = " + iterationResult);
+		}catch (IllegalArgumentException e) {
+			System.out.println(e.getMessage());
+		}
+		
 	}
-	
+
 	// iteration algorithm
 	public static long iterationFactorial(int number) {
-		long result = 1;		
+		long result = 1;
+		if (number < 0) {
+			throw new IllegalArgumentException("Iteration. Argument can't be less than zero");
+		}
 		for (int i = 1; i <= number; i++) {
 			result *= i;
 		}
 		return result;
 	}
+
 	// recursive algorithm
 	public static long recursiveFactorial(int number) {
+		if (number < 0) {
+			throw new IllegalArgumentException("Recursive. Argument can't be less than zero");
+		}
 		if (number == 0) {
 			return 1;
 		} else {
